2076b2ef18f29a02fb081ba179afd985d957711b
김성수 <85716720+SsangSoo@users.noreply.github.com>
2023-05-31 18:37:57 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 # 빌드 파일의 이름이 콘텐츠와 다르다면 다음 줄의 .jar 파일 이름을 수정하시기 바랍니다.
 BUILD_JAR=$(ls /home/ubuntu/WanderHub_Backend/server/build/libs/server-0.0.1-SNAPSHOT.jar)
-JAR_NAME=$(basename $BUILD_JAR)
+JAR_NAME=server-0.0.1-SNAPSHOT
 
-echo "> 현재 시간: $(date)" >> /home/ubuntu/WanderHub_Backend/deploy.log
+echo "> 현재 시간: $(date)" >> /home/ubuntu/wanderHub-back/log/deploy.log
 
-echo "> build 파일명: $JAR_NAME" >> /home/ubuntu/WanderHub_Backend/deploy.log
+echo "> build 파일명: $JAR_NAME" >> /home/ubuntu/wanderHub-back/log/deploy.log
 
-echo "> build 파일 복사" >> /home/ubuntu/WanderHub_Backend/deploy.log
-DEPLOY_PATH=/home/ubuntu/WanderHub_Backend/
+echo "> build 파일 복사" >> /home/ubuntu/wanderHub-back/deploy.log
+DEPLOY_PATH=/home/ubuntu/wanderHub-back/
 cp $BUILD_JAR $DEPLOY_PATH
 
-echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/WanderHub_Backend/deploy.log
+echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/wanderHub-back/log/deploy.log
 CURRENT_PID=$(pgrep -f $JAR_NAME)
 
 if [ -z $CURRENT_PID ]
 then
-  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ubuntu/WanderHub_Backend/deploy.log
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ubuntu/wanderHub-back/log/deploy.log
 else
-  echo "> kill -9 $CURRENT_PID" >> /home/ubuntu/WanderHub_Backend/deploy.log
+  echo "> kill -9 $CURRENT_PID" >> /home/ubuntu/wanderHub-back/log/deploy.log
   sudo kill -9 $CURRENT_PID
   sleep 5
 fi
 
 
-DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/WanderHub_Backend/deploy.log
-sudo nohup java -jar $DEPLOY_JAR >> /home/ubuntu/WanderHub_Backend/deploy.log 2>/home/ubuntu/WanderHub_Backend/deploy_err.log &
+DEPLOY_JAR=$BUILD_JAR
+echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/wanderHub-back/log/deploy.log
+sudo nohup java -jar $DEPLOY_JAR >> /home/ubuntu/wanderHub-back/log/deploy.log 2>/home/ubuntu/wanderHub-back/log/deploy_err.log &
