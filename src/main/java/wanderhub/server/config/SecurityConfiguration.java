390273722aeaf88ebba4dfa33f1951a4ac8cc448
김성수 <85716720+SsangSoo@users.noreply.github.com>
2023-06-05 16:48:51 +0900

    dev To main (#25)
    * ci/ce pleasegit status!
    
    * appspec.yml fix
    
    * yml port setting (#16)
    
    * Fix/port set (#19)
    
    * yml port setting
    
    * port set
    
    * Fix/port set (#22)
    
    * yml port setting
    
    * port set
    
    * port 80 remove
    
    * OAuth redirection OK (#24)

---
+++ src/main/java/wanderhub/server/config/SecurityConfiguration.java
@@ -0,0 +1,31 @@
+package wanderhub.server.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration  // 구성정보 클래스
+public class SecurityConfiguration {
+    private String vender = "kakao";
+
+    @Value("${spring.security.oauth2.client.registration."+vender+".clientId}")
+    private String clientId;
+
+    @Bean   // 스프링에서 관리하는 빈으로 설정
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // 스프링 시큐리티 FilterChain 구성 // HttpSecurity를 통해서 HTTP요청에 대한 보안 설정을 구ㅅㅇ한다.
+        http
+                .formLogin().disable()  // formLogin 비활성화 // 우리 프로젝트에 자체 회원가입은 후순위
+                .httpBasic().disable()  // request전송마다 Username/Password 정보를 Header에 실어서 인증하는 방식 // 사용 안 하므로 disable
+                .oauth2Login(withDefaults());   //  OAuth2Login 인증 활성화
+
+//        http
+//                .authorizeRequests() // 접근 허용 request 설정해야함.
+
+        return http.build(); // SecurityFilterChain 구성
+    }
+}
