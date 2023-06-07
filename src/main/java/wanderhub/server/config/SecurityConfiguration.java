0dd96a71ea7ebb58f6189e745188544af81f5e01
SsangSoo <kss10080@naver.com>
2023-06-08 01:30:21 +0900

    OK

--- src/main/java/wanderhub/server/config/SecurityConfiguration.java
+++ src/main/java/wanderhub/server/config/SecurityConfiguration.java
@@ -5,8 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
