7240c54ea74ab05c1a841f59c9216db712faaa5e
SsangSoo <kss10080@naver.com>
2023-06-08 14:40:09 +0900

    KAKAO Complete

--- src/main/java/wanderhub/server/global/test/HomeTestController.java
+++ src/main/java/wanderhub/server/global/test/HomeTestController.java
@@ -1,14 +1,12 @@
-package wanderhub.server.global.home;
+package wanderhub.server.global.test;
 
-import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HomeController {
+public class HomeTestController {
     @Value("${spring.security.oauth2.client.registration.kakao.clientId}")
     private String kid;
 
