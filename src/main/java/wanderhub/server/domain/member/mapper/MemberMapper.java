308036a3cde38081a2e446b790f96dac9404ca2d
Haeun Kim <127297705+HaniiiK@users.noreply.github.com>
2023-06-19 23:44:11 +0900

    Update MemberMapper.java

--- src/main/java/wanderhub/server/domain/member/mapper/MemberMapper.java
+++ src/main/java/wanderhub/server/domain/member/mapper/MemberMapper.java
@@ -7,25 +7,22 @@ import wanderhub.server.domain.member.entity.Member;
 @Mapper(componentModel = "spring")
 public interface MemberMapper {
 
-    default Member patchMemberToMember(MemberDto.Patch patchMember) {
-        return Member.builder()
-                        .name(patchMember.getName())
-                        .nickName(patchMember.getNickName())
-                        .imgUrl(patchMember.getImgUrl())
-                        .local(patchMember.getLocal())
-                        .build();
-
-    }
-
+    Member memberDtoPatchToMember(MemberDto.Patch memberDtoPatch);
     default MemberDto.Response memberToMemberResponse(Member member) {
-        return MemberDto.Response.builder()
-                .Id(member.getId())
-                .name(member.getName())
-                .email(member.getEmail())
-                .nickName(member.getNickName())
-                .imgUrl(member.getImgUrl())
-                .local(member.getLocal())
-                .memberStatus(member.getMemberStatus())
-                .build();
+        if(member == null) {
+            return null;
+        } else {
+            MemberDto.Response.ResponseBuilder response = MemberDto.Response.builder();
+            response.Id(member.getId());
+            response.email(member.getEmail());
+            response.name(member.getName());
+            response.nickName(member.getNickName());
+            response.imgUrl(member.getImgUrl());
+            response.local(member.getLocal());
+            response.memberStatus(member.getMemberStatus());
+            response.createdAt(member.getCreatedAt());
+            response.modifiedAt(member.getModifiedAt());
+            return response.build();
+        }
     }
 }
