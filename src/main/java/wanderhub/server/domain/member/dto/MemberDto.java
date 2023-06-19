c5088b767e2a732726f6d796f44f3f373c2fde81
Haeun Kim <127297705+HaniiiK@users.noreply.github.com>
2023-06-19 23:41:39 +0900

    Update MemberDto.java

--- src/main/java/wanderhub/server/domain/member/dto/MemberDto.java
+++ src/main/java/wanderhub/server/domain/member/dto/MemberDto.java
@@ -1,14 +1,10 @@
 package wanderhub.server.domain.member.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-import org.springframework.lang.Nullable;
+import lombok.*;
 import wanderhub.server.domain.member.entity.MemberStatus;
 
-import javax.validation.constraints.NotBlank;
+import javax.persistence.Lob;
+import java.time.LocalDateTime;
 
 @NoArgsConstructor
 public class MemberDto {
@@ -20,20 +16,26 @@ public class MemberDto {
     public static class Patch {
         private String name;            // 이름 / null 허용
         private String nickName;     // 닉네임 / null, 공백, 빈문자 X
+        @Lob
         private String imgUrl;          // 이미지 Url  / null 허용
         private String local;           // 지역 / null 허용
     }
 
-    @AllArgsConstructor
     @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
     public static class Response {
         private Long Id;
         private String name;
         private String email;
         private String nickName;
+        @Lob
         private String imgUrl;
         private String local;
         private MemberStatus memberStatus;
+        private LocalDateTime createdAt;
+        private LocalDateTime modifiedAt;
     }
 
 }
