9112ee97f8f5a625d5e0093207fd6021d1e36ddc
Haeun Kim <127297705+HaniiiK@users.noreply.github.com>
2023-06-19 23:32:21 +0900

    Update ServerApplication.java

--- src/main/java/wanderhub/server/ServerApplication.java
+++ src/main/java/wanderhub/server/ServerApplication.java
@@ -3,8 +3,6 @@ package wanderhub.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import wanderhub.server.domain.member.entity.Member;
-import wanderhub.server.domain.member.service.MemberService;
 
 @EnableJpaAuditing // JPA의 Auditing 기능 활성화
 @SpringBootApplication
