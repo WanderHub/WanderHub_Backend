33c097a5ab5667c6009c22a9b10a89ce9c8320e6
SsangSoo <kss10080@naver.com>
2023-06-20 16:34:10 +0900

    ok

--- src/main/java/wanderhub/server/auth/handler/OAuth2MemberSuccessHandler.java
+++ src/main/java/wanderhub/server/auth/handler/OAuth2MemberSuccessHandler.java
@@ -117,16 +117,16 @@ public class OAuth2MemberSuccessHandler extends SimpleUrlAuthenticationSuccessHa
 
 
 //         http://localhost/receive-token?access_token=accessToken&refresh_token=refreshToken
-        return UriComponentsBuilder
-                .newInstance()
-                .scheme("http")
-                .host("localhost")
-                .port(8080)
-                .path("/receive-token")
-                .queryParams(queryParams)
-                .build()
-                .toUri();
-    }
+//        return UriComponentsBuilder
+//                .newInstance()
+//                .scheme("http")
+//                .host("localhost")
+//                .port(8080)
+//                .path("/receive-token")
+//                .queryParams(queryParams)
+//                .build()
+//                .toUri();
+//    }
 
 //        return UriComponentsBuilder
 //                .newInstance()
@@ -140,16 +140,16 @@ public class OAuth2MemberSuccessHandler extends SimpleUrlAuthenticationSuccessHa
 //    }
 
 
-//        return UriComponentsBuilder
-//                .newInstance()
-//                .scheme("https")
-//                .host("wanderHub.kro.kr")
-//                .port(443)
-//                .path("/receive-token")
-//                .queryParams(queryParams)
-//                .build()
-//                .toUri();
-//
-//    }
+        return UriComponentsBuilder
+                .newInstance()
+                .scheme("https")
+                .host("backwander.kro.kr")
+                .port(443)
+                .path("/receive-token")
+                .queryParams(queryParams)
+                .build()
+                .toUri();
+
+    }
 
 }
