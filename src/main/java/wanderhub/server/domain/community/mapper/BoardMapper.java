a5d426a2714e8c2da3b5adcdd390a3195aec473b
SsangSoo <kss10080@naver.com>
2023-06-21 11:18:51 +0900

    fix/bug board and member

--- src/main/java/wanderhub/server/domain/community/mapper/BoardMapper.java
+++ src/main/java/wanderhub/server/domain/community/mapper/BoardMapper.java
@@ -3,14 +3,50 @@ package wanderhub.server.domain.community.mapper;
 import org.mapstruct.Mapper;
 import wanderhub.server.domain.community.dto.BoardDto;
 import wanderhub.server.domain.community.entity.Board;
+import wanderhub.server.global.utils.Local;
 
 @Mapper(componentModel = "spring")
 public interface BoardMapper {
 
-    Board boardPostDtoToBoard(BoardDto.Post post);
+    default Board boardPostDtoToBoard(BoardDto.Post post) {
+        if(post==null) {
+            return null;
+        } else {
+            return Board.builder()
+                    .title(post.getTitle())
+                    .content(post.getContent())
+                    .local(Local.findByLocal(post.getLocal()))
+                    .build();
+        }
+    }
 
-    Board boardPatchDtoToBoard(BoardDto.Patch patch);
+    default Board boardPatchDtoToBoard(BoardDto.Patch patch) {
+        if(patch==null) {
+            return null;
+        } else {
+            return Board.builder()
+                    .title(patch.getTitle())
+                    .content(patch.getContent())
+                    .local(Local.findByLocal(patch.getLocal()))
+                    .build();
+        }
+    }
 
-    BoardDto.Response boardToBoardResponseDto(Board board);
+    default BoardDto.Response boardToBoardResponseDto(Board board) {
+        if(board==null) {
+            return null;
+        } else {
+            return BoardDto.Response.builder()
+                    .boardId(board.getBoardId())
+                    .nickName(board.getNickName())
+                    .title(board.getTitle())
+                    .content(board.getContent())
+                    .local(board.getLocal().getLocal())
+                    .viewPoint(board.getViewPoint())
+                    .createdAt(board.getCreatedAt())
+                    .modifiedAt(board.getModifiedAt())
+                    .build();
+        }
+    }
 
 }
