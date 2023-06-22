d5e8ef7f06b43117fd67251a84d320387889d1f3
HaeunKim <rlagkdms26@gmail.com>
2023-06-22 13:12:33 +0900

    test/accompany
    * 테스트코드 조금 수정

--- src/test/java/wanderhub/server/domain/accompany/mapper/AccompanyMapperTest.java
+++ src/test/java/wanderhub/server/domain/accompany/mapper/AccompanyMapperTest.java
@@ -1,44 +1,67 @@
-//package wanderhub.server.domain.accompany.mapper;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-//import wanderhub.server.domain.accompany.dto.AccompanyResponseDto;
-//import wanderhub.server.domain.accompany.entity.Accompany;
-//import wanderhub.server.domain.accompany.repository.AccompanyRepository;
-//
-//import java.time.LocalDate;
-//import java.util.List;
-//
-//@DataJpaTest
-//public class AccompanyMapperTest {
-//
-//    private final AccompanyRepository repo;
-//
-//    @Autowired
-//    public AccompanyMapperTest(AccompanyRepository repo) {
-//        this.repo = repo;
-//    }
-//
-//    @Test
-//    public void create() {
-//        Accompany accompany = new Accompany(null, 4L, "hi1", "서울", LocalDate.parse("2023-06-01"), 3, "제목", "내용", true);
-//        repo.save(accompany);
-//        AccompanyResponseDto accompanyDto = AccompanyMapper.INSTANCE.toDto(accompany);
-//    }
-//
-//    @Test
-//    public void findAll() {
-//        //더미데이터 저장
-//        Accompany accompany1 = new Accompany(null, 5L, "user5", "제주", LocalDate.parse("2023-06-05"), 2, "제목1", "내용1", true);
-//        repo.save(accompany1);
-//        Accompany accompany2 = new Accompany(null, 6L, "user6", "서울", LocalDate.parse("2023-06-07"), 4, "제목2", "내용2", true);
-//        repo.save(accompany2);
-//
-//        List<Accompany> entityList = repo.findAll();
-//        List<AccompanyResponseDto> dtoList = AccompanyMapper.INSTANCE.toDtoList(entityList);
-//        System.out.println("dtoList = " + dtoList);
-//    }
-//
-//
-//}
+package wanderhub.server.domain.accompany.mapper;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import wanderhub.server.domain.accompany.dto.AccompanyResponseDto;
+import wanderhub.server.domain.accompany.entity.Accompany;
+import wanderhub.server.domain.accompany.repository.AccompanyRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@DataJpaTest
+public class AccompanyMapperTest {
+
+    private final AccompanyRepository repo;
+
+    @Autowired
+    public AccompanyMapperTest(AccompanyRepository repo) {
+        this.repo = repo;
+    }
+
+    @Test
+    public void create() {
+        Accompany accompany = Accompany.builder()
+                        .nickname("nickname")
+                        .accompanyLocal("서울")
+                        .accompanyDate(LocalDate.parse("2023-06-01"))
+                        .maxNum(3)
+                        .accompanyTitle("제목")
+                        .accompanyContent("내용")
+                        .openStatus(true)
+                        .build();
+        repo.save(accompany);
+        AccompanyResponseDto responseDto = AccompanyMapper.INSTANCE.toDto(accompany);
+        Assertions.assertThat(responseDto.getNickname()).isEqualTo(accompany.getNickname());
+    }
+
+    @Test
+    public void findAll() {
+        Accompany accompany1 = Accompany.builder()
+                .nickname("nickname")
+                .accompanyLocal("서울")
+                .accompanyDate(LocalDate.parse("2023-06-01"))
+                .maxNum(3)
+                .accompanyTitle("제목")
+                .accompanyContent("내용")
+                .openStatus(true)
+                .build();
+        Accompany accompany2 = Accompany.builder()
+                .nickname("nickname2")
+                .accompanyLocal("제주도")
+                .accompanyDate(LocalDate.parse("2023-07-01"))
+                .maxNum(6)
+                .accompanyTitle("제목2")
+                .accompanyContent("내용2")
+                .openStatus(true)
+                .build();
+        repo.save(accompany1);
+        repo.save(accompany2);
+
+        List<Accompany> entityList = repo.findAll();
+        List<AccompanyResponseDto> dtoList = AccompanyMapper.INSTANCE.toDtoList(entityList);
+        Assertions.assertThat(dtoList.get(0).getNickname()).isEqualTo(accompany1.getNickname());
+    }
+}
